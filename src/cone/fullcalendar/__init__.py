acd6d82d899370126fa1ca31fba0f64240bc8796
Johannes Raggam <thetetet@gmail.com>
2017-01-16 19:03:39 +0100

    register resources

--- src/cone/fullcalendar/__init__.py
+++ src/cone/fullcalendar/__init__.py
@@ -8,14 +8,15 @@ logger = logging.getLogger('cone.fullcalendar')
 
 # protected CSS
 css = cone.app.cfg.css
-css.protected.append('calendar-static/fullcalendar.min.css')
-css.protected.append('calendar-static/fullcalendar.print.css')
+css.protected.append('calendar-static/fullcalendar/fullcalendar.min.css')
+css.protected.append('calendar-static/fullcalendar/fullcalendar.print.css')
 css.protected.append('calendar-static/calendar.css')
 
 
 # protected JS
 js = cone.app.cfg.js
-js.protected.append('calendar-static/fullcalendar.min.js')
+js.protected.append('calendar-static/moment/moment.min.js')
+js.protected.append('calendar-static/fullcalendar/fullcalendar.min.js')
 js.protected.append('calendar-static/calendar.js')
 
 
