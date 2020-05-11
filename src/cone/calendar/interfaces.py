82c039d730abb8e71208a7adc65322efb5d813f9
Robert Niederreiter <office@squarewave.at>
2020-05-11 20:41:45 +0200

    calendar interface naming

--- src/cone/calendar/interfaces.py
+++ src/cone/calendar/interfaces.py
@@ -2,13 +2,13 @@ from zope.interface import Attribute
 from zope.interface import Interface
 
 
-class IScheduled(Interface):
+class IEvent(Interface):
     start = Attribute('Date as datetime.datetime instance')
     end = Attribute('Date as datetime.datetime instance')
 
 
-class IRecurring(Interface):
+class ICron(Interface):
     effective = Attribute('Date as datetime.datetime instance')
     expires = Attribute('Date as datetime.datetime instance')
-    cron_rule = Attribute('Crontab rule as string')
+    rule = Attribute('Crontab rule as string')
     duration = Attribute('Duration in minutes')
