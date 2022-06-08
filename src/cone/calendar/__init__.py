44304430967e1a5ae4a117db4ede41ff9005788f
Robert Niederreiter <rnix@squarewave.at>
2022-06-08 08:40:25 +0200

    Register resources with webresource

--- src/cone/calendar/__init__.py
+++ src/cone/calendar/__init__.py
@@ -1,6 +1,6 @@
 from cone.app import cfg
 from cone.app import main_hook
-from cone.calendar.browser import static_resources
+from cone.calendar.browser import configure_resources
 import logging
 
 
@@ -11,29 +11,11 @@ logger = logging.getLogger('cone.calendar')
 def initialize_calendar(config, global_config, settings):
     # application startup initialization
 
-    # protected CSS
-    cfg.css.protected.append('calendar-static/fullcalendar/fullcalendar.min.css')
-    # XXX: print CSS needs to be delivered dynamically in dedicated print view
-    # cfg.css.protected.append('calendar-static/fullcalendar/fullcalendar.print.css')
-    cfg.css.protected.append('calendar-static/calendar.css')
-
-    # protected JS
-    cfg.js.protected.append('calendar-static/moment/moment.min.js')
-    cfg.js.protected.append('calendar-static/fullcalendar/fullcalendar.min.js')
-
-    locales = settings.get('cone.calendar.locales')
-    if locales:
-        for locale in [loc.strip() for loc in locales.split(',') if loc]:
-            locale_js = 'calendar-static/fullcalendar/locale/{}.js'.format(locale)
-            cfg.js.protected.append(locale_js)
-
-    cfg.js.protected.append('calendar-static/calendar.js')
-
     # add translation
     config.add_translation_dirs('cone.calendar:locale/')
 
     # static resources
-    config.add_view(static_resources, name='calendar-static')
+    configure_resources(settings)
 
     # scan browser package
     config.scan('cone.calendar.browser')
--- src/cone/calendar/browser/__init__.py
+++ src/cone/calendar/browser/__init__.py
@@ -1,6 +1,88 @@
 from .calendar import CalendarEvents  # noqa
 from .calendar import CalendarTile  # noqa
+from cone.app.browser.resources import resources
+from cone.app.browser.resources import set_resource_include
 from pyramid.static import static_view
+import os
+import webresource as wr
 
 
-static_resources = static_view('static', use_subpath=True)
+resources_dir = os.path.join(os.path.dirname(__file__), 'static')
+
+
+moment_resources = wr.ResourceGroup(
+    name='cone.calendar-moment',
+    directory=os.path.join(resources_dir, 'moment'),
+    path='moment',
+    group=resources
+)
+moment_resources.add(wr.ScriptResource(
+    name='moment-js',
+    resource='moment.min.js'
+))
+
+fullcalendar_resources = wr.ResourceGroup(
+    name='cone.calendar-fullcalendar',
+    directory=os.path.join(resources_dir, 'fullcalendar'),
+    path='fullcalendar',
+    group=resources
+)
+fullcalendar_resources.add(wr.ScriptResource(
+    name='fullcalendar-js',
+    depends=['moment-js', 'jquery-js'],
+    resource='fullcalendar.js',
+    compressed='fullcalendar.min.js'
+))
+fullcalendar_resources.add(wr.StyleResource(
+    name='fullcalendar-css',
+    resource='fullcalendar.css',
+    compressed='fullcalendar.min.css'
+))
+# XXX: print CSS needs to be delivered dynamically in dedicated print view
+# fullcalendar_resources.add(wr.StyleResource(
+#     name='fullcalendar-print-css',
+#     resource='fullcalendar.print.css',
+#     compressed='fullcalendar.print.min.css'
+# ))
+
+cone_calendar_resources = wr.ResourceGroup(
+    name='cone.calendar-calendar',
+    directory=resources_dir,
+    path='calendar',
+    group=resources
+)
+cone_calendar_resources.add(wr.ScriptResource(
+    name='cone-calendar-js',
+    depends='fullcalendar-js',
+    resource='calendar.js',
+    compressed='calendar.min.js'
+))
+cone_calendar_resources.add(wr.StyleResource(
+    name='cone-calendar-css',
+    depends='fullcalendar-css',
+    resource='calendar.css'
+))
+
+
+def configure_resources(settings):
+    set_resource_include(settings, 'moment-js', 'authenticated')
+
+    set_resource_include(settings, 'fullcalendar-js', 'authenticated')
+    set_resource_include(settings, 'fullcalendar-css', 'authenticated')
+
+    set_resource_include(settings, 'cone-calendar-js', 'authenticated')
+    set_resource_include(settings, 'cone-calendar-css', 'authenticated')
+
+    locales = settings.get('cone.calendar.locales')
+    if not locales:
+        return
+    locales_directory = os.path.join(resources_dir, 'fullcalendar', 'locale')
+    for locale in [loc.strip() for loc in locales.split(',') if loc]:
+        locale_name = 'fullcalendar-{}-js'.format(locale)
+        fullcalendar_resources.add(wr.ScriptResource(
+            name=locale_name,
+            depends='fullcalendar-js',
+            directory=locales_directory,
+            resource='{}.js'.format(locale)
+        ))
+        set_resource_include(settings, locale_name, 'authenticated')
